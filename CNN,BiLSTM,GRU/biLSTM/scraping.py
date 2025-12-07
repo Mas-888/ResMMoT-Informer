3045d36a7f67a52ac3522617140487c5dfc38d10
Lzk2297 <Lzk2297@outlook.com>
2025-12-07 15:28:30 +0800

    Add files via upload

---
+++ CNN,BiLSTM,GRU/biLSTM/scraping.py
@@ -0,0 +1,11 @@
+# Version python3.6
+# -*- coding: utf-8 -*-
+# @Time    : 2018/1/19 下午3:13
+# @Author  : zenRRan
+# @Email   : zenrran@qq.com
+# @File    : scraping.py
+# @Software: PyCharm Community Edition
+
+import numpy as np
+a = np.zeros((2, 2))
+
